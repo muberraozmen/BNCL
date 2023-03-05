7d94ae6dd5f1af6dd95eff9be9ca35ed49cba717
muberraozmen <babam1harika>
2023-03-05 06:15:13 -0500

    transformation for stackex_philosophy

--- transformation/converter.py
+++ transformation/converter.py
@@ -25,7 +25,7 @@ class DataConverter(object):
                 Y.append(self.onehot(tgt))
                 counter = counter + 1
                 print(counter)
-                if counter % 10 == 0:
+                if counter % 100 == 0:
                     self.X = torch.stack(X, dim=0)
                     self.Y = torch.stack(Y, dim=0)
                     self.save_data(data_dir + '/first' + str(counter) + '/')

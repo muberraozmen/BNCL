6c9ebbf82d7bbc62652694d2b5145619c2b02d40
muberraozmen <babam1harika>
2023-04-23 18:43:00 -0400

    glove vs bert edits

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 results_dir="./update/outputs/reuters/glove/"
 for i in 1 2 3
 do
-  for j in 0
+  for j in 0 2 4 8 16
   do
     python3.8 -u update/train.py -supervision $i -seed $j -results_dir $results_dir
   done
